d45b78e6eb01b2f88c86055043c3acc2674e1556
Sam Lee <68556120+jvhaa@users.noreply.github.com>
2026-01-16 16:31:46 -0500

    Add decryption script for Big Bang Secrets

---
+++ J4/CCC '12 J4 - Big Bang Secrets.py
@@ -0,0 +1,13 @@
+k = int(input())
+cipher = input()
+
+plain = ""
+for i in range(len(cipher)):
+    letter = cipher[i:i+1]
+    shift = 3 * (i+1) + k
+    x = ord(letter) - shift
+    if x < ord("A"):
+        x = ord("Z") + x - ord("A") + 1
+    plain += chr(x)
+
+print(plain)
